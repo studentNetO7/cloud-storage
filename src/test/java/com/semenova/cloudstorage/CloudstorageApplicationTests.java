ed5665a5b4fbe5eb86e90d758e53494262072669
Olga7 test <7enovaoa@gmail.com>
2025-05-07 14:57:37 +0100

    Initial commit: project structure and configuration

---
+++ src/test/java/com/semenova/cloudstorage/CloudstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semenova.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudstorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
